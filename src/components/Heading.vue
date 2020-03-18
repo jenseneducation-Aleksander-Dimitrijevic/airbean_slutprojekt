00324e5f229c30212de500101c7324d72d548754
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2020-03-18 20:59:52 +0100

    added menu route + heading component

---
+++ src/components/Heading.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="heading">
+    <h1>{{ heading }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Heading",
+  props: {
+    heading: String
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.heading {
+  margin: 1rem 0;
+  text-align: center;
+
+  h1 {
+    font-size: 3rem;
+  }
+}
+</style>
---
+++ src/views/Menu.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="menu">
+    <div
+      class="top-image"
+      :style="{ backgroundImage: 'url(' + require('@/assets/img/graphics-header.svg') + ')' }"
+    ></div>
+    <Heading heading="Meny" />
+  </div>
+</template>
+
+<script>
+import Heading from "@/components/Heading.vue";
+export default {
+  name: "Menu",
+  components: {
+    Heading
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.top-image {
+  width: 100%;
+  height: 80px;
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+</style>
