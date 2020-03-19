05e84025a6347ecdd81c4763a9db56ea7cee9df5
Elam Nabi <elam@nabi.nu>
2020-03-19 21:07:12 +0100

    Fixed the landing-page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,29 @@
 <template>
-  <div class="left-image-container">
-    <h1>AIR BEAN</h1>
-    <h6>D R O N E D E L I V E R E D C O F F E E</h6>
-    <div class="right-image-container"></div>
+  <div id="landing-page">
+    <img :src="require('@/assets/img/airbean-landing.svg')" alt="logo" />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Home"
+};
 </script>
 
-<style lang="scss">
-.left-image-container {
-  background: url("~@/assets/img/intro-graphic-left.svg") no-repeat top/50%;
+<style lang="scss" scoped>
+#landing-page {
+  background: url("~@/assets/img/intro-graphic-left.svg"),
+    url("~@/assets/img/intro-graphic-right.svg");
+
   height: 100vh;
   width: 100%;
-
-  h1,
-  h6 {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: rgb(255, 255, 255);
-  }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-position-x: -70px, 70px;
+  background-color: #39846d;
 }
 </style>
