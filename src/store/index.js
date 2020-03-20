5f7be483819abe948f817e2f191600e3b18ba4b5
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2020-03-20 22:20:33 +0100

    style the cart box

--- src/store/index.js
+++ src/store/index.js
@@ -6,14 +6,18 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     items: [],
-    isOpen: false
+    isOpen: false,
+    cartOpen: false
   },
   mutations: {
     ADD_NEW_ITEM(state, newItem) {
       state.items.push(newItem);
     },
     TOGGLE_MENU(state) {
-      state.isOpen = !state.isOpen
+      state.isOpen = !state.isOpen;
+    },
+    TOGGLE_CART(state) {
+      state.cartOpen = !state.cartOpen;
     }
   },
   actions: {
@@ -21,7 +25,10 @@ export default new Vuex.Store({
       context.commit("ADD_NEW_ITEM", newItem);
     },
     toggleMenu(context) {
-      context.commit('TOGGLE_MENU')
+      context.commit("TOGGLE_MENU");
+    },
+    toggleCart(context) {
+      context.commit("TOGGLE_CART");
     }
   }
 });
